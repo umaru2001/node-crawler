ef96125697f1e9f634bd78bb1e1a366d7891cd5b
pengdonglai <pengdonglai@bytedance.com>
2023-03-26 17:42:03 +0800

    feat: 增加了对数据库的搜索支持

--- app.js
+++ app.js
@@ -10,11 +10,17 @@ const app = express();
 //   port: 3306
 // })
 
-const { getTestArticle } = services;
+const { getTestArticle, getArticleResearch } = services;
 
 app.get('/test', function (req, response) {
   console.log('客户端请求了/test接口!');
   getTestArticle(response);
+});
+
+app.get('/chairman/search/:data', function (req, response) {
+  console.log('客户端请求了/search接口!');
+  const { data } = req.params;
+  getArticleResearch(response ,data);
 });
 
 app.listen('3000', function () {
--- services.js
+++ services.js
@@ -1,5 +1,4 @@
 const cheerio = require('cheerio');
-const express = require('express');
 const http = require('http');
 
 function responseArticle(response, title, origin, time, content) {
@@ -11,14 +10,18 @@ function responseArticle(response, title, origin, time, content) {
   });
 }
 
+function responseArticles(response, articleData) {
+  response && response.send(articleData);
+}
+
 async function getTestArticle(response) {
 
   const req = http.request('http://jhsjk.people.cn/article/32634480?isindex=1', (res) => {
-    let chunks = [];
+    const chunks = [];
     res.on('data', c => chunks.push(c));
     res.on('end', () => {
-      let htmlStr = Buffer.concat(chunks).toString('utf-8');
-      let $ = cheerio.load(htmlStr);
+      const htmlStr = Buffer.concat(chunks).toString('utf-8');
+      const $ = cheerio.load(htmlStr);
       const title = $($('.d2txt.clearfix > h1')[0]).text();
       const subTitle = $($('.d2txt.clearfix > .d2txt_1.clearfix')[0]).text().split(' ');
       const [origin, time] = [subTitle[0].split('：')[1].trim(), subTitle[subTitle.length - 1].split('：')[1].trim()];
@@ -30,6 +33,51 @@ async function getTestArticle(response) {
   req.end();
 }
 
+async function getTestArticles(response, urlList) {
+  const innerUrlList = urlList.slice(0, 3);
+  const articleData = [];
+  innerUrlList.forEach(url => {
+    const req = http.request(`http://jhsjk.people.cn/${url}?isindex=1`, (res) => {
+      const chunks = [];
+      res.on('data', c => chunks.push(c));
+      res.on('end', () => {
+        const htmlStr = Buffer.concat(chunks).toString('utf-8');
+        const $ = cheerio.load(htmlStr);
+        const title = $($('.d2txt.clearfix > h1')[0]).text();
+        const subTitle = $($('.d2txt.clearfix > .d2txt_1.clearfix')[0]).text().split(' ');
+        const [origin, time] = [subTitle[0].split('：')[1].trim(), subTitle[subTitle.length - 1].split('：')[1].trim()];
+        const content = $($('.d2txt.clearfix > .d2txt_con.clearfix')[0]).html().trim();
+        articleData.push({ title, time, origin, content });
+        if (articleData.length === 3) {
+          responseArticles(response, articleData);
+        }
+      });
+    })
+    req.end();
+  });
+}
+
+async function getArticleResearch(response, keyword) {
+
+  const urlList = [];
+
+  const req = http.request(`http://jhsjk.people.cn/result?searchArea=0&keywords=${keyword}&isFuzzy=1`, (res) => {
+    const chunks = [];
+    res.on('data', c => chunks.push(c));
+    res.on('end', () => {
+      const htmlStr = Buffer.concat(chunks).toString('utf-8');
+      const $ = cheerio.load(htmlStr);
+      $($('#news_list')[0]).find('li').each(function () {
+        urlList.push($($(this).find('a')[0]).attr('href'));
+      });
+      getTestArticles(response, urlList);
+    });
+  });
+
+  req.end();
+}
+
 module.exports = {
   getTestArticle,
+  getArticleResearch,
 }
